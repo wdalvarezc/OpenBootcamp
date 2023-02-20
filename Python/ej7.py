3b88fbfd47d8dc2b12c2007d4a0de294628d21f6
William Alvarez <granadarecords18@gmail.com>
2023-02-20 17:30:59 -0500

    ej7

---
+++ Python/ej7.py
@@ -0,0 +1,10 @@
+import modEj7 as o
+
+def main():
+    print(o.suma(4,5))
+    print(o.resta(14,5))
+    print(o.multiplicacion(4,5))
+    print(o.division(10,5))
+
+if __name__ == '__main__':
+    main()
